a9f1f9865d8dbd6a0f91fcbdc0feae30aa5e199e
Nimeavles <prlf505@gmail.com>
2025-03-14 18:18:15 +0100

    feat(ui): status of channels

---
+++ app/frontend/src/components/InformationCard.tsx
@@ -0,0 +1,24 @@
+import { JSX } from "react";
+
+interface Props {
+  name: string;
+  status: string;
+  icon: JSX.Element;
+  className?: string;
+}
+
+export const InformationCard = ({ name, status, icon, className }: Props) => {
+  return (
+    <div
+      className={`w-full md:flex-1/2 flex items-center gap-5 px-5 py-2 lg:px-7 lg-py:3 rounded ${className}`}
+    >
+      {icon}
+      <span className="flex flex-col">
+        <p className="text-md font-medium text-black dark:text-gray-300">
+          {name}
+        </p>
+        <p className="font-bold text-xl">{status}</p>
+      </span>
+    </div>
+  );
+};
--- app/frontend/src/pages/HomePage.tsx
+++ app/frontend/src/pages/HomePage.tsx
@@ -1,13 +1,35 @@
+import { InformationCard } from "@/components/InformationCard";
 import { PageLayout } from "@/components/PageLayout";
+import { Activity, CircleAlert, CircleCheckBig, Signal } from "lucide-react";
 
 export const HomePage = () => {
   return (
     <PageLayout>
-      <div className="flex flex-col items-center justify-center mt-12">
-        <h1 className="text-4xl font-bold">Acestream Scraper</h1>
-        <p className="text-lg mt-4">
-          Welcome to the Acestream Scraper homepage.
-        </p>
+      <div className="flex flex-col md:flex-row gap items-center justify-center mt-6 gap-4">
+        <InformationCard
+          name="Channels"
+          icon={<Signal />}
+          status="0"
+          className="bg-accent-lg text-fuchsia-300 dark:text-primary border-fuchsia-200 dark:bg-accent border dark:border-primary"
+        />
+        <InformationCard
+          name="Checked"
+          icon={<CircleCheckBig />}
+          status="0"
+          className="dark:bg-checked border dark:border-blue-900 bg-checked-lg border-blue-200 dark:text-blue-800 text-blue-300"
+        />
+        <InformationCard
+          name="Online"
+          icon={<Activity />}
+          status="0"
+          className="dark:bg-online bg-online-lg border dark:border-green-900 border-green-200 dark:text-green-800 text-green-400"
+        />
+        <InformationCard
+          name="Offline"
+          icon={<CircleAlert />}
+          status="0"
+          className="dark:bg-offline bg-offline-lg border dark:border-red-900 border-red-200 dark:text-red-800 text-red-400"
+        />
       </div>
     </PageLayout>
   );
