b2306096ba3db68fdc23f6a562118c38bfbdb87d
Nimeavles <prlf505@gmail.com>
2025-04-15 11:15:31 +0200

    fix(ui): use system color mode as default theme

--- app/frontend/src/hooks/useDarkTheme.tsx
+++ app/frontend/src/hooks/useDarkTheme.tsx
@@ -2,8 +2,13 @@ import { useState } from "react";
 
 type ThemeOptions = "light" | "dark";
 
+const systemCurrentColor = () => {
+  const colorScheme = window.matchMedia("(prefers-color-scheme: dark)");
+  return colorScheme.matches ? "dark" : "light";
+};
+
 export const useTheme = () => {
-  const [theme, setTheme] = useState<ThemeOptions>("dark");
+  const [theme, setTheme] = useState<ThemeOptions>(systemCurrentColor());
 
   const toggleTheme = () => {
     setTheme(theme === "dark" ? "light" : "dark");
